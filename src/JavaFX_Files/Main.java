db9379a3faddcbdb5998307839ce07c565398898
Soheeb Amin <soheeb@gmail.com>
2020-10-26 18:56:13 -0400

    made GUI elements for entry scene

--- src/JavaFX_Files/Main.java
+++ src/JavaFX_Files/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainScreenGUI.fxml"));
-        primaryStage.setTitle("");
-        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.setTitle("Inventory Management System");
+        primaryStage.setScene(new Scene(root, 900, 500));
         primaryStage.show();
     }
 
