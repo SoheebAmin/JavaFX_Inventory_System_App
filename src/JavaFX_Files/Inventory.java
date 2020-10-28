5b1600cedba0ecadbd6c5cee76ef4ab1f79bac4c
Soheeb Amin <soheeb@gmail.com>
2020-10-27 21:46:38 -0400

    more done for Inventory

--- src/JavaFX_Files/Inventory.java
+++ src/JavaFX_Files/Inventory.java
@@ -9,6 +9,14 @@ public class Inventory {
     private static ObservableList<Part> allParts = FXCollections.observableArrayList();
     private static ObservableList<Product> allProducts = FXCollections.observableArrayList();
 
+    public static void addPart(Part part) {
+        allParts.add(part);
+    }
+
+    public static void addProduct(Product product) {
+        allProducts.add(product);
+    }
+
     public static ObservableList<Part> getAllParts() {
         return allParts;
     }
