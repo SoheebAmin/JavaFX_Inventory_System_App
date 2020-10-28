51596d8206c3e110c60b7142528101552c2fd4a8
Soheeb Amin <soheeb@gmail.com>
2020-10-27 21:41:50 -0400

    all classes made

---
+++ src/JavaFX_Files/Inventory.java
@@ -0,0 +1,19 @@
+package JavaFX_Files;
+
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Inventory {
+
+    private static ObservableList<Part> allParts = FXCollections.observableArrayList();
+    private static ObservableList<Product> allProducts = FXCollections.observableArrayList();
+
+    public static ObservableList<Part> getAllParts() {
+        return allParts;
+    }
+
+    public static ObservableList<Product> getAllProducts() {
+        return allProducts;
+    }
+}
--- src/JavaFX_Files/inHouse.java
+++ src/JavaFX_Files/inHouse.java
@@ -1,7 +1,20 @@
 package JavaFX_Files;
 
 public class inHouse extends Part{
-    public inHouse(int id, String name, double price, int stock, int min, int max) {
+
+    private int machineId;
+
+
+    public inHouse(int id, String name, double price, int stock, int min, int max, int machineId) {
         super(id, name, price, stock, min, max);
+        this.machineId = machineId;
+    }
+
+    public int getMachineId() {
+        return machineId;
+    }
+
+    public void setMachineId(int machineId) {
+        this.machineId = machineId;
     }
 }
