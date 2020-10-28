c9e735d19a75913a77e99e8cbf884b11eab263ba
Soheeb Amin <soheeb@gmail.com>
2020-10-27 21:18:38 -0400

    created part superclass, started its sub-classes

---
+++ src/JavaFX_Files/inHouse.java
@@ -0,0 +1,7 @@
+package JavaFX_Files;
+
+public class inHouse extends Part{
+    public inHouse(int id, String name, double price, int stock, int min, int max) {
+        super(id, name, price, stock, min, max);
+    }
+}
