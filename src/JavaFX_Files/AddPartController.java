8301e1c09057a3b7a06d9a9641621c5fa958bca4
Soheeb Amin <soheeb@gmail.com>
2020-10-29 20:26:42 -0400

    refactored opening new window

--- src/JavaFX_Files/AddPartController.java
+++ src/JavaFX_Files/AddPartController.java
@@ -42,28 +42,24 @@ public class AddPartController implements Initializable{
         // add it to the Inventory observable list, so it saved and displayed in GUI.
         Inventory.addPart(new inHouse(id, name, price, inventory, min, max, machineId));
 
-        // gets the scene to load and sets it in a variable
-        Parent MainScreenParent = FXMLLoader.load(getClass().getResource(("MainScreenGUI.fxml")));
-        Scene MainScreenScene = new Scene(MainScreenParent);
-
-        // sets new scene into the window
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();   // need this to get the stage info
-        window.setScene(MainScreenScene);
-        window.show();
+        changeScene(event, "MainScreenGUI.fxml");
     }
 
 
     // The method to cancel and return to the previous scene
     public void cancelButtonClicked(ActionEvent event) throws IOException {
+        changeScene(event, "MainScreenGUI.fxml");
+    }
+
+    public void changeScene(ActionEvent event, String sceneName) throws IOException {
         // gets the scene to load and sets it in a variable
-        Parent MainScreenParent = FXMLLoader.load(getClass().getResource(("MainScreenGUI.fxml")));
+        Parent MainScreenParent = FXMLLoader.load(getClass().getResource((sceneName)));
         Scene MainScreenScene = new Scene(MainScreenParent);
 
         // sets new scene into the window
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();   // need this to get the stage info
         window.setScene(MainScreenScene);
         window.show();
-
     }
 
     @Override
